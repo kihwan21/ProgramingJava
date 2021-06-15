0247e775d3f2be7e80ac73a43b6388f3260a6ecc
kihwan <kimki8121@gmail.com>
2021-06-15 18:15:10 +0900

    Have to code in below

--- Worker.java
+++ Worker.java
@@ -29,32 +29,10 @@ public class Worker {
 		String Rank = scanner.next();
 		
 		while(true) { // 계속 반복			
-			if(EmployeeName.equals("quit") || (DPT.equals("quit") { // quit와 같다면 while문을 빠져나옴
-				scanner.close();
-				break;
-			}
-			if(DPT.equals("quit")) { // quit와 같다면 while문을 빠져나옴
-				scanner.close();
-				break;
-			}
-			if(CompanyNum.equals("quit")) { // quit와 같다면 while문을 빠져나옴
-				scanner.close();
-				break;
-			}
-			if(POS.equals("quit")) { // quit와 같다면 while문을 빠져나옴
-				scanner.close();
-				break;
-			}
-			if(Rank.equals("quit")) { // quit와 같다면 while문을 빠져나옴
-				scanner.close();
-				break;
-			}
-			
-			
+			//선 표시
 			for(int i=1; i<=10; i++) {
 				System.out.print("=");
 			}
-			
 			
 			System.out.println();
 			System.out.print("이름:");
@@ -65,50 +43,70 @@ public class Worker {
 			System.out.println(CompanyNum);
 			System.out.print("직책:");
 			System.out.println(POS);
-			System.out.print("업무 성적:");
+			System.out.print("업무성적:");
 			System.out.println(Rank);
+		
 			
 			for(int i=1; i<=10; i++) {
 				System.out.print("=");
 			}
 			
-			//여기에다 넣어보기ㄴㄴㄴ
+			//여기에다 넣어보기
+			System.out.println();
+			System.out.println("직원의 이름, 부서, 사번, 직책, 업무 성적을 입력하시오.");
+			
+			System.out.println();
 			
 			
 			
-			EmployeeName = scanner.next();  //입력 값 초기화 부분
+			EmployeeName = scanner.next();  //입력 값 초기화 부분  scanner가 계속 사용자의 입력을 받고 있어서 quit를 입력하더라도 끝까지 입력을 끝내야 종료되는 문제가 있음.
 			DPT = scanner.next();
 			CompanyNum = scanner.next();
 			POS = scanner.next();
 			Rank = scanner.next();
 			
-//			System.out.println();
-//			System.out.println("직원의 이름, 부서, 사번, 직책, 업무 성적을 입력하시오.");
-//			
-//			System.out.println();
-//			
-			//초기화 부분
 			
 			
+			if(EmployeeName.equals("quit")) { // quit와 같다면 while문을 빠져나옴
+				scanner.close();
+				break;
+			}
+			else if(DPT.equals("quit")) { // quit와 같다면 while문을 빠져나옴
+				scanner.close();
+				break;
+			}
+			else if(CompanyNum.equals("quit")) { // quit와 같다면 while문을 빠져나옴
+				scanner.close();
+				break;
+			}
+			else if(POS.equals("quit")) { // quit와 같다면 while문을 빠져나옴
+				scanner.close();
+				break;
+			}
+			else if(Rank.equals("quit")) { // quit와 같다면 while문을 빠져나옴
+				scanner.close();
+				break;
+			}
+			System.out.println();
+			System.out.println("직원의 이름, 부서, 사번, 직책, 업무 성적을 입력하시오.");
+			
+			System.out.println();
 				
 		}
-		System.out.println("Exit Program"); //break에서 탈출하면 출력
+		System.out.println("확인하고 싶은 직원 이름 입력"); //break에서 탈출하면 출력
+		String check = scanner.next();
+		if(check.equals("None")) {
+			System.out.println("Exit Program");
+			scanner.close();
+		}
+		// 여기서 입력 받은거 내놓기
+		
+		
+		
 	}
 }
 
 
-//else {
-//	System.out.println(num +"=> dosen't exist in the dictionary");
-//	System.out.print("Input word[if you want to quit, input \"stop\"]>>");
-//	num = scanner.next();  //입력 값 초기화 부분
-//	continue;
-//}
 
-//int array[] = new int[]; //배열선언
-//
-//for(int i=0; i<array.length; i++) {
-//	array[i]=(int)(Math.random()*10+1);
-//	System.out.print(array[i]+"\t");
-//	}
-//	System.out.println("");
+
 
